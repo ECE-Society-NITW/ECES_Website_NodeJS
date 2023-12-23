2d5e12e8027a98a66e3917655ef9e6e1cee3e0cb
shashaank1453 <sd21ecb0a56@student.nitw.ac.in>
2023-12-23 07:07:53 +0530

    Cancel Registration API Added

--- controllers/event_controller.js
+++ controllers/event_controller.js
@@ -1,11 +1,11 @@
-const EventModel = require("../models/event");
+const EventModel = require("../models/event")
 const EventController = {
   getEvent: async function (req, res) {
     try {
-      const events = await EventModel.find();
-      return res.json({ success: true, data: events });
+      const events = await EventModel.find()
+      return res.json({ success: true, data: events })
     } catch (ex) {
-      return res.json({ success: false, message: ex });
+      return res.json({ success: false, message: ex })
     }
   },
   addEvent: async function (req, res) {
@@ -18,7 +18,7 @@ const EventController = {
         description,
         targetAudience,
         createdOn,
-      } = req.body;
+      } = req.body
       const newEvent = new EventModel({
         title,
         photo: photo || "",
@@ -27,90 +27,115 @@ const EventController = {
         description: description || "",
         targetAudience: targetAudience || 0,
         createdOn: createdOn || new Date(),
-      });
+      })
 
-      await newEvent.save();
+      await newEvent.save()
 
       return res.json({
         success: true,
         data: newEvent,
         message: "Event added successfully",
-      });
+      })
     } catch (ex) {
-      return res.json({ success: false, message: ex });
+      return res.json({ success: false, message: ex })
     }
   },
 
   updateEvent: async function (req, res) {
     try {
-        const event_id = req.params.eventId;
-        const updatedEvent = await EventModel.findOneAndUpdate(
-          { event_id },
-          req.body,
-          { new: true }
-        );
+      const event_id = req.params.eventId
+      const updatedEvent = await EventModel.findOneAndUpdate(
+        { event_id },
+        req.body,
+        { new: true }
+      )
 
       return res.json({
         success: true,
         data: updatedEvent,
         message: "Event updated successfully",
-      });
+      })
     } catch (ex) {
-      return res.json({ success: false, message: ex });
+      return res.json({ success: false, message: ex })
     }
   },
 
   removeEvent: async function (req, res) {
     try {
-      const event_id = req.params.eventId;
-      const removedEvent = await EventModel.findOneAndDelete({event_id});
+      const event_id = req.params.eventId
+      const removedEvent = await EventModel.findOneAndDelete({ event_id })
       if (!removedEvent) {
-        return res.json({ success: false, message: "Event not found" });
+        return res.json({ success: false, message: "Event not found" })
       }
       return res.json({
         success: true,
         data: removedEvent,
         message: "Event removed successfully",
-      });
+      })
     } catch (ex) {
-      return res.json({ success: false, message: ex });
+      return res.json({ success: false, message: ex })
     }
   },
   addUser: async function (req, res) {
     try {
-      const { email, name } = req.body.user;
-      const event_id = req.params.eventId;
+      const { email, name } = req.body.user
+      const event_id = req.params.eventId
       const isUserRegistered = await EventModel.exists({
         event_id,
         'registeredUsers.email': email,
-      });
-  
+      })
+
       if (isUserRegistered) {
         return res.json({
           success: false,
           message: 'Already registered!',
-        });
+        })
       }
-  
       const updatedEvent = await EventModel.findOneAndUpdate(
         { event_id },
         { $push: { registeredUsers: { email, name } } },
         { new: true }
-      );
-  
+      )
+
       if (!updatedEvent) {
-        return res.json({ success: false, message: 'Event not found' });
+        return res.json({ success: false, message: 'Event not found' })
       }
-  
+
       return res.json({
         success: true,
         message: `Successfully registered to ${updatedEvent.title}`,
-      });
+      })
     } catch (ex) {
-      return res.json({ success: false, message: ex });
+      return res.json({ success: false, message: ex })
     }
   },
-  
-};
+  removeUser: async function (req, res) {
+    try {
+      const { email } = req.body.user
+      const event_id = req.params.eventId
+      const event = await EventModel.findOne({ event_id })
+      if (!event)
+        return res.json({ success: false, message: 'Event not found' })
+      const isUserRegistered = event.registeredUsers.some((user) => user.email === email)
+      if (!isUserRegistered) {
+        return res.json({
+          success: false,
+          message: `Not registered for this event`,
+        })
+      }
+      const updatedEvent = await EventModel.findOneAndUpdate(
+        { event_id },
+        { $pull: { registeredUsers: { email } } },
+        { new: true }
+      )
+      return res.json({
+        success: true,
+        message: `Cancelled Registration ${updatedEvent.title}`,
+      })
+    } catch (ex) {
+      return res.json({ success: false, message: ex })
+    }
+  }
+}
 
-module.exports = EventController;
+module.exports = EventController
